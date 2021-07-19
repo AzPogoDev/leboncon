52a538c48cfc5c4769b169f334635c76ed2364a6
henri <henricousin07@gmail.com>
2021-07-19 13:46:52 +0200

    dz

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = parse_url(getenv("postgres://sdfesvnqsrvuct:570c0d8089b6eba4b318b124ef30d1b5c7f20e38eaa4ff3211903371fa25e4e5@ec2-54-217-195-234.eu-west-1.compute.amazonaws.com:5432/d4p9o9uhhjkbfb"));
-
+var_dump($db);
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
