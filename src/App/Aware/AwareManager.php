3f65c5c6ca032ec9b6b235bf9ecfd9682a155d7d
henri <henricousin07@gmail.com>
2021-07-19 11:41:59 +0200

    Adding db

--- src/App/Aware/AwareManager.php
+++ src/App/Aware/AwareManager.php
@@ -40,7 +40,7 @@ class AwareManager
     {
         if ($object instanceof PdoAware) {
             if (!$this->pdo) {
-                $this->pdo = new PDO('mysql:host=localhost;dbname=leboncon', 'root');
+                $this->pdo = new PDO('postgres://sdfesvnqsrvuct:570c0d8089b6eba4b318b124ef30d1b5c7f20e38eaa4ff3211903371fa25e4e5@ec2-54-217-195-234.eu-west-1.compute.amazonaws.com:5432/d4p9o9uhhjkbfb');
             }
 
             $object->setPdo($this->pdo);
